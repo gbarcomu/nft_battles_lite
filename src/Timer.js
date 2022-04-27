844ee9a59f74d9b515ec582798bdfd8f424370f3
Marcos Luis Delgado <marcosluisdelgado@gmail.com>
2022-04-27 17:45:12 +0200

    Update Timer.js

--- src/Timer.js
+++ src/Timer.js
@@ -1,20 +1,19 @@
 import { useEffect, useState } from 'react';
 
 function Timer(props) {
-
-    //const remainingTime = await getPlayerRemainingTime();
     const [counter, setCounter] = useState(props.remainingTime);
 
     useEffect(() => {
-        //getPlayerRemainingTime().then(val => console.log(val));
-        const timer =
-            counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-        return () => clearInterval(timer);
-    }, [counter]);
+        let timer;
+        getPlayerRemainingTime().then((val) => {
+            // El timer se setea asincronamente, cambiara cuando se complete la promesa
+            setCounter(val);
+            timer = counter > 0 && setInterval(() => setCounter((prevCounter) => prevCounter - 1), 1000); // Usar el valor previo
+        });
+        return () => timer && clearInterval(timer);
+    }, []); // Sin dependecias para que solo se lance en el mount
 
-    return (
-        <div>Countdown: {counter}</div>
-    );
+    return <div>Countdown: {counter}</div>;
 }
 
 export { Timer }
