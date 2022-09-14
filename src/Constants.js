e3f960e8c191b8eaa0c8a1ba18fd7bd01cee6bc8
gbarcomu <gbarcomu@gmail.com>
2022-09-14 19:20:16 +0200

    adding mainnet

--- src/Constants.js
+++ src/Constants.js
@@ -8,11 +8,20 @@
 // export const CHAIN_NAME = 'Localhost 8545';
 // export const RPC_URL = 'http://localhost:8545';
 //Mumbai
-export const NFT_SQUAD_ADDRESS = "0xdD67e8e841800FdeeBb227434eb0977E7FA04609";
-export const QUEST_MANAGER_ADDRESS = "0x6AAEC13A252883E9D5Dd7B687564dA2AD4D325e7";
-export const LOOT_TOKEN_ADDRESS = "0x50F707Acf06c3BB789800c978e01F2b65813A76B";
-export const MINT_PRICE = "0.05";
-export const MEAL_PRICE = "0.01";
-export const CHAIN_ID =  '0x13881';
-export const CHAIN_NAME = 'Mumbai Testnet';
-export const RPC_URL = 'https://matic-mumbai.chainstacklabs.com';
+// export const NFT_SQUAD_ADDRESS = "0xdD67e8e841800FdeeBb227434eb0977E7FA04609";
+// export const QUEST_MANAGER_ADDRESS = "0x6AAEC13A252883E9D5Dd7B687564dA2AD4D325e7";
+// export const LOOT_TOKEN_ADDRESS = "0x50F707Acf06c3BB789800c978e01F2b65813A76B";
+// export const MINT_PRICE = "0.05";
+// export const MEAL_PRICE = "0.01";
+// export const CHAIN_ID =  '0x13881';
+// export const CHAIN_NAME = 'Mumbai Testnet';
+// export const RPC_URL = 'https://matic-mumbai.chainstacklabs.com';
+//Polygon
+export const NFT_SQUAD_ADDRESS = "0x98835d4CB70bCB023d9879B85455403277CbfD92";
+export const QUEST_MANAGER_ADDRESS = "0xF6b7Ea577B5EB40D89ba0eBD31D6088cF7b0CD93";
+export const LOOT_TOKEN_ADDRESS = "0xdc13E943A1D616218C36E0C0790efEAff8aEe427";
+export const MINT_PRICE = "5.0";
+export const MEAL_PRICE = "1.0";
+export const CHAIN_ID =  '0x89';
+export const CHAIN_NAME = 'Polygon Mainnet';
+export const RPC_URL = 'https://polygon-rpc.com';
